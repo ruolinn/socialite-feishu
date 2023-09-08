0198655c0144c5279a82768e1ff0dc9ff9924a9d
王晓光 <wangxiaoguang@wangxiaoguangdeMac-mini.local>
2023-09-08 14:23:04 +0800

    update

--- src/Provider.php
+++ src/Provider.php
@@ -3,6 +3,7 @@
 namespace SocialiteProviders\FeiShu;
 
 use Illuminate\Support\Arr;
+use GuzzleHttp\RequestOptions;
 use Laravel\Socialite\Two\InvalidStateException;
 use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
@@ -30,13 +31,70 @@ class Provider extends AbstractProvider
         return 'https://open.feishu.cn/open-apis/authen/v1/access_token';
     }
 
+    protected function getTokenHeaders($code)
+    {
+        return [
+            'Accept'       => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+    }
+
+    protected function getTokenFields($code)
+    {
+        return [
+            'grant_type' => 'authorization_code',
+            'code' => $code,
+            'app_id' => $this->clientId,
+            'app_secret' => $this->clientSecret,
+        ];
+    }
+
     protected function getUserByToken($token)
     {
+        $response = $this->getHttpClient()->get(
+            'https://open.feishu.cn/open-apis/authen/v1/user_info',
+            [
+                RequestOptions::HEADERS => ['Authorization' => 'Bearer '.$token]
+            ]);
 
+        return json_decode($response->getBody(), true);
     }
 
     protected function mapUserToObject(array $user)
     {
+        $user = $user['data'];
 
+        return (new User)->setRaw($user)->map([
+            'id' => $user['open_id'],
+            'name' => $user['name'],
+            'email' => $user['email'],
+            'nickname' => $user['en_name'],
+            'avatar' => $user['avatar_big'],
+        ]);
+    }
+
+    protected function parseAccessToken($body)
+    {
+        return Arr::get($body['data'], 'access_token');
+    }
+
+    protected function parseRefreshToken($body)
+    {
+        return Arr::get($body['data'], 'refresh_token');
+    }
+
+    protected function parseExpiresIn($body)
+    {
+        return Arr::get($body['data'], 'refresh_expires_in');
+    }
+
+    public function getAccessTokenResponse($code)
+    {
+        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+            RequestOptions::HEADERS => $this->getTokenHeaders($code),
+            RequestOptions::JSON => $this->getTokenFields($code),
+        ]);
+
+        return json_decode($response->getBody(), true);
     }
 }
